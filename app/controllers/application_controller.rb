ff2714c4013ca30383382cfb6b4f9a7d49c82744
github username <github email address>
2018-10-17 14:01:18 -0400

    Root Page

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+    end
+    
+  end
+
+end

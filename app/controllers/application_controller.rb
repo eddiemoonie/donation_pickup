87f184fe96fb6ad0d433f99dbabb97a460d78e52
github username <github email address>
2018-10-17 15:35:28 -0400

    Log Out and User Show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "donationpickup"
   end
 
   get '/' do
@@ -14,8 +16,13 @@ class ApplicationController < Sinatra::Base
   helpers do
 
     def logged_in?
+      !!session[:user_id]
     end
-    
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,61 @@
 class UsersController < ApplicationController
 
+  get '/users/:id' do
+    if !logged_in?
+      erb :'users/login'
+    end
+
+    @user = User.find(params[:id])
+    if !@user.nil? && @user == current_user
+      erb :'users/show'
+    else
+      redirect to '/login'
+    end
+  end
+
   get '/signup' do
-    erb :'/users/new'
+    if !session[:user_id]
+      erb :'users/new'
+    else
+      redirect to "/users/#{@user.id}"
+    end
   end
 
   post '/signup' do
     if params[:username] == "" || params[:password] == ""
-      redirect "/signup"
+      redirect '/signup'
     else
       @user = User.create(:username => params[:username], :password => params[:password])
       session[:user_id] = @user.id
+      redirect to "/users/#{@user.id}"
     end
   end
+
+  get '/login' do
+    if !session[:user_id]
+      erb :'/users/login'
+    else
+      redirect to "/users/#{@user.id}"
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/users/#{@user.id}"
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    if session[:user_id] != nil
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end 
 
 end

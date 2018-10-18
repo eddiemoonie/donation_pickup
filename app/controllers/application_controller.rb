d63b4dc785404c6b94203f68755e926cd0144bd5
github username <github email address>
2018-10-18 17:36:34 -0400

    unfinished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "donationpickup"
+    set :session_secret, "felp"
   end
 
   get '/' do

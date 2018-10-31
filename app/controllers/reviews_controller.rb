846bc331f6c790037c8f6c6a99482461974736cd
github username <github email address>
2018-10-31 18:10:44 -0400

    Review view button and show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,10 @@
 class ReviewsController < ApplicationController
 
-  get '/restaurants/:slug/edit' do
+  get '/restaurants/:slug/reviews/:id' do
+    redirect_if_not_logged_in
     @restaurant = Restaurant.find_by_slug(params[:slug])
+    @review = Review.find(params[:id])
+    erb :'reviews/show'
   end
 
 end

a4dd96a5b8bb86b7849a5715860c281a55317478
github username <github email address>
2018-11-01 14:09:08 -0400

    Review Edit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,24 @@ class ReviewsController < ApplicationController
     erb :'reviews/show'
   end
 
+  get '/restaurants/:slug/reviews/:id/edit' do
+    redirect_if_not_logged_in
+    @restaurant = Restaurant.find_by_slug(params[:slug])
+    @review = Review.find(params[:id])
+    erb :'reviews/edit'
+  end
+
+  patch '/restaurants/:slug/reviews/:id' do
+    redirect_if_not_logged_in
+    @restaurant = Restaurant.find_by_slug(params[:slug])
+    @review = Review.find(params[:id])
+    if @restaurant && @review && !current_user.admin?
+      @review.update(
+        :content => params[:content],
+        :rating => params[:rating]
+      )
+      redirect to "/restaurants/#{@restaurant.slug}/reviews/#{@review.id}"
+    end
+  end
+
 end
